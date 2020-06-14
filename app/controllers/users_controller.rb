55d316f50cd10d762c941463a95a91bce7859642
vidurpunj <vidur.punj@hotmail.com>
2020-06-14 23:27:47 +0530

    Master update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def shared_locations_with_me
-    @user_locations = SharedLocation.joins('left join users on shared_locations.shared_with_id = users.id')
+    @user_locations = SharedLocation.joins('left join users on shared_locations.user_id = users.id')
                           .joins('left join user_locations on user_locations.id = shared_locations.user_location_id')
                           .select('shared_locations.id, user_locations.latitude, user_locations.longitude, user_locations.address, users.email')
                           .where('shared_locations.shared_with_id = ?', current_user.id)
